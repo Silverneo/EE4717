b8a05849224e4157f73b2bc87a95c8c5b8f9ea1e
Chunmeng <cmzhang7@hotmail.com>
2014-10-09 00:42:40 +0800

    Fixed some bugs

--- register.php
+++ register.php
@@ -1,11 +1,12 @@
 <!DOCYTYPE html>
 <html lang="en">
 <head>
-<title>Try Us</title>
+<title>Register Result</title>
 <meta charset="utf-8">
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="wrapper">
 <header>
 <div id="logo-box">
 <img id="logo" src="images/logo.png">
@@ -25,7 +26,7 @@
 <div class="clear"></div>
 </nav>
 </header>
-<div class = "container">
+<div class = "content">
 <?php // register.php
 include "conn_f31s23.php";
 if (isset($_POST['submit'])) {
@@ -59,13 +60,12 @@ else
 
 </div>
 <footer>
-<div>
 <ul>
 <li>Contact Us</li>
 <li>Membership</li>
 <li>Sign up</li>
 </ul>
-<p style="float: left"><small>Copyright &copy; Fit-Tastic!<small></p>
+<p style="float: left"><small>Copyright &copy; Fit-Tastic!</small></p>
 <div class="clear"></div>
 <ul id="footer-social">
 <li><A href="http://www.facebook.com"><IMG src="images/facebook.jpg" alt="Facebook" id = "social-logo"></a></li>
@@ -73,7 +73,6 @@ else
 <li><A href="http://www.youtube.com"><IMG src="images/Youtube.jpg" alt="Youtube" id = "social-logo"></a></li>
 </ul>
 <div class="clear"></div>
-<div>
 </footer>
 </div>
 </body>
--- sign-in.php
+++ sign-in.php
@@ -1,11 +1,12 @@
 <!DOCYTYPE html>
 <html lang="en">
 <head>
-<title>Try Us</title>
+<title>Sign-in Result</title>
 <meta charset="utf-8">
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="wrapper">
 <header>
 <div id="logo-box">
 <img id="logo" src="images/logo.png">
@@ -30,7 +31,7 @@
 <div class="clear"></div>
 </nav>
 </header>
-<div class="container">
+<div class="content">
 <?php //sign-in.php
 
 session_start();
@@ -74,13 +75,12 @@ $db_conn->close();
 ?>
 </div>
 <footer>
-<div>
 <ul>
 <li>Contact Us</li>
 <li>Membership</li>
 <li>Sign up</li>
 </ul>
-<p style="float: left"><small>Copyright &copy; Fit-Tastic!<small></p>
+<p style="float: left"><small>Copyright &copy; Fit-Tastic!</small></p>
 <div class="clear"></div>
 <ul id="footer-social">
 <li><A href="http://www.facebook.com"><IMG src="images/facebook.jpg" alt="Facebook" id = "social-logo"></a></li>
@@ -88,7 +88,6 @@ $db_conn->close();
 <li><A href="http://www.youtube.com"><IMG src="images/Youtube.jpg" alt="Youtube" id = "social-logo"></a></li>
 </ul>
 <div class="clear"></div>
-<div>
 </footer>
 </div>
 </body>
